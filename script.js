2d80e4f7b80ca8d5a4a873ad02c3b8687e034324
Aboullah Balde <abdoullahbalde@gmail.com>
2023-06-09 11:46:33 +0200

    condition javascript

---
+++ script.js
@@ -0,0 +1,68 @@
+// plusieres if else
+
+// let age = 96;
+// if (age <= 2) {
+//   console.log("bonjour bebe");
+// } else if (age <= 10) {
+//   console.log("bonjour adolescant");
+// } else if (age <= 20) {
+//   console.log("bonjour mineur");
+// } else if (age <= 60) {
+//   console.log("bonjour vieux");
+// } else if (age > 90) {
+//   console.log("bonjour vieux");
+// } else {
+//   console.log("au revoir");
+// }
+
+//les conditions switch
+
+let number = 10;
+switch (number) {
+  case 1:
+    console.log("janvier");
+    break;
+  case 2:
+    console.log("fevrier");
+    break;
+
+  case 3:
+    console.log("mars");
+    break;
+
+  case 4:
+    console.log("Avril");
+    break;
+
+  case 5:
+    console.log("mai");
+    break;
+
+  case 6:
+    console.log("juin");
+    break;
+  case 7:
+    console.log("juillet");
+    break;
+  case 8:
+    console.log("Aout");
+    break;
+  case 9:
+    console.log("septembre");
+    break;
+
+  case 10:
+    console.log("Octobre");
+    break;
+
+  case 11:
+    console.log("novembre");
+    break;
+
+  case 12:
+    console.log("Decembre");
+    break;
+
+  default:
+    console.log("nombre invalide");
+}
